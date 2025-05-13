7238a9cdda9e7e5e0adaf6e6e3e086a282b34261
anna luiza moura da silva <anna.moura.silva@escola.pr.gov.br>
2025-05-13 11:32:58 -0300

    capitulo 1 exemplo 1

---
+++ pythonProject2/capitulo 1/exemplo1operadoresaritimeticos.py
@@ -0,0 +1,14 @@
+# Operador de soma
+soma = 1 + 2
+
+# Operador de subtração
+subtracao = 2 - 1
+
+#operador de Multiplicaçao
+multiplicacao = 2 * 1
+
+# operador de divisao
+divisao = 1 / 2
+
+#operador de exponenciacao
+exponenciacao = 2 ** 1
