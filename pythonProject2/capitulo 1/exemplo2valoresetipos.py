e21782ca6943ff93a21fa0d08d7eb6497a535405
anna luiza moura da silva <anna.moura.silva@escola.pr.gov.br>
2025-05-20 11:37:08 -0300

    cap1 exemplo2

---
+++ pythonProject2/capitulo 1/exemplo2valoresetipos.py
@@ -0,0 +1,16 @@
+# tipo inteiro 1, 2, 3 ...
+inteiro = 2
+print(type(inteiro))
+
+# tipo real ou ponto flutuante float 1.3, 0.6... -3.456...
+float = 1.3
+print(type(float))
+
+#characteres
+char1 = 'a'
+char2 = '2'
+print(type(char1))
+
+#string conjunto de characteres
+stringNome = "Jose Valim"
+print(type(stringNome))
